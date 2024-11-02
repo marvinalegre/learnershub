ceb2eef0c787980e47facd71170c446cb12031d0
Marvin Alegre <marvinalegredev@gmail.com>
2024-11-02 11:51:22 +0800

    refactor: use react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./style.css";
 
-import NavBar from "./components/NavBar";
+import Root from "./routes/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NavBar />
+    <RouterProvider router={router} />
   </StrictMode>
 );
